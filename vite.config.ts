eddbdf820b2bea19648062b112fd3591808d944c
Hiron Das <hcdas.09@gmail.com>
2023-05-26 21:43:03 +0600

    base is added in defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  base: "fem-expenses-chart-component",
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
